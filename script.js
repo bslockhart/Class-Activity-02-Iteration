1423c13af30431bff9f1b43020616605e3e345f4
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-10-26 17:12:56 -0400

    updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // TODO: Write Your JavaScript Code Here
+// var helloWorld = "Hello World";
+// var manyHellos = ["Hello World", "I am here", "This is now"];
+
+var classMates = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"];
+// console.log(classMates[0]);
+// console.log(classMates[1]);
+// console.log(classMates[2]);
+
+for (var i = 0; i < classMates.length; i++) {
+    console.log('Number, ' + classMates[i] + '!');
+}
+
+// for (var i = 0; i < 7; i++) {
+//     console.log('Number, ' + classMates[i] + '!');
+// }
